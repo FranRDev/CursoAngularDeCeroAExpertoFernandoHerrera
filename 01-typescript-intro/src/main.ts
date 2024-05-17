ec36cf51cf66d95f8cd4352cdabff280fa774584
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-17 11:02:22 +0200

    Clase 19. Tarea sobre objetos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
-import './temas/01-tipos-basicos'
-import './temas/02-interfaz-objeto'
-import './temas/03-funciones'
+// import './temas/01-tipos-basicos'
+// import './temas/02-interfaz-objeto'
+// import './temas/03-funciones'
+import './temas/04-tarea-tipos'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola mundo
