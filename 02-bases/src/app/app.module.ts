f51572547c3784b1822ed541bb9cff5f85d5018f
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-20 13:07:59 +0200

    Clase 52. Tarea sobre módulos

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListaComponent } from './heroes/lista/lista.component';
 import { ContadorModule } from './contador/componentes/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent,
-    ListaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ContadorModule
+    ContadorModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
