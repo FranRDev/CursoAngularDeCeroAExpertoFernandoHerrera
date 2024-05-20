1a0db8f9a9e425079157caf4cc5424cc6c9366ff
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-20 10:57:15 +0200

    Clase 41. Contador

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   public title: string = 'Hola mundo';
   public contador: number = 10;
+
+  incrementarPor(valor: number): void {
+    this.contador += valor;
+  }
+
+  reiniciar() {
+    this.contador = 10;
+  }
+
 }
