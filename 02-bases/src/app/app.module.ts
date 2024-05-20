2188bced0e9ac9f5be823aedd0cd4f51acddcf3d
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-20 11:48:58 +0200

    Clase 44. Componente Hero y directorios

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListaComponent } from './heroes/lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
