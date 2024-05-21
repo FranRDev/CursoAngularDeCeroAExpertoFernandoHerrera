5dcb4874ae0288656f311b9eb11438cbf4b43c07
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-21 14:54:42 +0200

    Clase 91. @ViewChild - Referencia al HTML

--- 03-gifs-app/src/app/gifs/components/buscador/buscador.component.ts
+++ 03-gifs-app/src/app/gifs/components/buscador/buscador.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'buscador-gifs',
   template: `
     <h5>Buscar: </h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs...">
+    <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="buscarEtiqueta()" #entradaEtiqueta>
   `
 })
 export class BuscadorComponent {
+
+  @ViewChild('entradaEtiqueta')
+  public entradaEtiqueta!: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  buscarEtiqueta(): void {
+    const etiqueta = this.entradaEtiqueta.nativeElement.value;
+    console.log({etiqueta});
+  }
 
 }
