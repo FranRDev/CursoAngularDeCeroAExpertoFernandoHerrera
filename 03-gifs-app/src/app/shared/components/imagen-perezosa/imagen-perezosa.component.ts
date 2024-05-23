7fcbe0742278a63c31aa58fb92faf81796026d8a
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-23 08:55:52 +0200

    Clase 109. Lazy Image - Parte 2

--- 03-gifs-app/src/app/shared/components/imagen-perezosa/imagen-perezosa.component.ts
+++ 03-gifs-app/src/app/shared/components/imagen-perezosa/imagen-perezosa.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'imagen-perezosa',
   templateUrl: './imagen-perezosa.component.html'
 })
-export class ImagenPerezosaComponent {
+export class ImagenPerezosaComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public cargada: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) { throw new Error('URL es requerido'); }
+  }
+
+  imagenCargada(): void {
+    console.log('Imagen cargada');
+    this.cargada = true;
+  }
 
 }
