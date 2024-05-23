b023616dcd184802492d993e4cb5594b6495c72b
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-23 14:14:04 +0200

    Clase . Countries Service - Restcountries

--- 04-paises-app/src/app/app.module.ts
+++ 04-paises-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
---
+++ 04-paises-app/src/app/paises/services/paises.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Pais } from '../interfaces/paises.interface';
+
+@Injectable({providedIn: 'root'})
+export class PaisesService {
+
+  private urlBase: string = 'https://restcountries.com';
+
+  constructor(private clienteHttp: HttpClient) { }
+
+  buscarCapital(busqueda: string): Observable<Pais[]> {
+    return this.clienteHttp.get<Pais[]>(`${this.urlBase}/capital/${busqueda}`);
+  }
+
+}
