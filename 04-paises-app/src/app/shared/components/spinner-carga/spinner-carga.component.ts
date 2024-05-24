5ee3bf494a60518e77972f1b7ad9d1defbdbbae3
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-24 10:48:39 +0200

    Clase 137. LoadingComponent - Mostrar un indicador de carga

---
+++ 04-paises-app/src/app/shared/components/spinner-carga/spinner-carga.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spinner-carga',
+  templateUrl: './spinner-carga.component.html',
+  styleUrls: ['./spinner-carga.component.css']
+})
+export class SpinnerCargaComponent {
+
+}
--- 04-paises-app/src/app/shared/shared.module.ts
+++ 04-paises-app/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { BarraLateralComponent } from './components/barra-lateral/barra-lateral.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { PaginaAcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 import { PaginaContactoComponent } from './pages/contacto/contacto.component';
 import { PaginaInicioComponent } from './pages/inicio/inicio.component';
-import { RouterModule } from '@angular/router';
+import { SpinnerCargaComponent } from './components/spinner-carga/spinner-carga.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     BuscadorComponent,
     PaginaAcercaDeComponent,
     PaginaContactoComponent,
-    PaginaInicioComponent
+    PaginaInicioComponent,
+    SpinnerCargaComponent
   ],
   imports: [
     CommonModule,
@@ -25,7 +27,8 @@ import { RouterModule } from '@angular/router';
     BuscadorComponent,
     PaginaAcercaDeComponent,
     PaginaContactoComponent,
-    PaginaInicioComponent
+    PaginaInicioComponent,
+    SpinnerCargaComponent
   ]
 })
 export class SharedModule { }
