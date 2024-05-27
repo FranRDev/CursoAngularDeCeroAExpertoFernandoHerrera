60def0120126024c85b7e8ba206edc5a38f9c1cd
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-27 09:18:21 +0200

    Corrección menú

--- 05-pipes-app/src/app/shared/components/menu/menu.component.ts
+++ 05-pipes-app/src/app/shared/components/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'menu',
+  selector: 'shared-menu',
   templateUrl: './menu.component.html',
   styles: ``
 })
