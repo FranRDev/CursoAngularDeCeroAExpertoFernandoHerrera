a219242aae57bd597ab70f780988add646f5df35
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-27 10:21:46 +0200

    Clase 160. Cambiar idioma global y manualmente

--- 05-pipes-app/src/app/app.module.ts
+++ 05-pipes-app/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+// Configuración de 'locale'.
+import localeES from '@angular/common/locales/es'
+import localeFrCA from '@angular/common/locales/fr-CA'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localeES);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
@@ -16,7 +24,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
