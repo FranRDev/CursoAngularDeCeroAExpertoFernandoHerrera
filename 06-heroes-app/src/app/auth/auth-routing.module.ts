cd87809bd7f3ee03fbe659abd69a354750b4e630
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-27 13:16:37 +0200

    Clase 186. Paths de las rutas faltantes

--- 06-heroes-app/src/app/auth/auth-routing.module.ts
+++ 06-heroes-app/src/app/auth/auth-routing.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from "@angular/core";
+import { Component, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { PaginaLayoutComponent } from "./pages/layout/layout.component";
+import { PaginaInicioSesionComponent } from "./pages/inicio-sesion/inicio-sesion.component";
+import { PaginaRegistroComponent } from "./pages/registro/registro.component";
 
-const rutas: Routes = [
-  {path: '', component: PaginaLayoutComponent}
+const routes: Routes = [
+  {
+    path: '',
+    component: PaginaLayoutComponent,
+    children: [
+      { path: 'inicio-sesion', component: PaginaInicioSesionComponent },
+      { path: 'registro', component: PaginaRegistroComponent },
+      { path: '**', redirectTo: 'inicio-sesion' }
+    ]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(rutas)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule { }
--- 06-heroes-app/src/app/heroes/heroes-routing.module.ts
+++ 06-heroes-app/src/app/heroes/heroes-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PaginaLayoutComponent } from './pages/layout/layout.component';
 import { PaginaAnhadirComponent } from './pages/anhadir/anhadir.component';
+import { PaginaBusquedaComponent } from './pages/busqueda/busqueda.component';
+import { PaginaHeroeComponent } from './pages/heroe/heroe.component';
+import { PaginaLayoutComponent } from './pages/layout/layout.component';
+import { PaginaListaComponent } from './pages/lista/lista.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PaginaLayoutComponent,
     children: [
-      { path: 'anhadir', component: PaginaAnhadirComponent }
+      { path: 'anhadir', component: PaginaAnhadirComponent },
+      { path: 'busqueda', component: PaginaBusquedaComponent },
+      { path: 'editar/:id', component: PaginaAnhadirComponent },
+      { path: 'lista', component: PaginaListaComponent },
+      { path: ':id', component: PaginaHeroeComponent },
+      { path: '**', redirectTo: 'lista' }
     ]
   }
 ];
