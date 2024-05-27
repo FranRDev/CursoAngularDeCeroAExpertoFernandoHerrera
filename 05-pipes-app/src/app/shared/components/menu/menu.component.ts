b10fbd574bf0985760fee54be1283d67505d45b9
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-27 08:39:51 +0200

    Clase 156. Navegar usando el MenuBar

--- 05-pipes-app/src/app/shared/components/menu/menu.component.ts
+++ 05-pipes-app/src/app/shared/components/menu/menu.component.ts
@@ -16,9 +16,9 @@ export class MenuComponent implements OnInit {
         label: 'Pipes de Angular',
         icon: 'pi pi-desktop',
         items: [
-          { label: 'Textos y flechas', icon: 'pi pi-align-left' },
-          { label: 'Números', icon: 'pi pi-dollar' },
-          { label: 'No comunes', icon: 'pi pi-globe' },
+          { label: 'Textos y flechas', icon: 'pi pi-align-left', routerLink: '/' },
+          { label: 'Números', icon: 'pi pi-dollar', routerLink: 'numeros' },
+          { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'poco-comun' },
         ]
       },
       {
