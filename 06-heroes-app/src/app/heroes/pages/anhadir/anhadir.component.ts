91c4f83981da5875b23166495396b073e6547446
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-28 11:31:41 +0200

    Clase 211. HeroForm - ReactiveForms introducción

--- 06-heroes-app/src/app/heroes/pages/anhadir/anhadir.component.ts
+++ 06-heroes-app/src/app/heroes/pages/anhadir/anhadir.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'pagina-anhadir',
-  templateUrl: './anhadir.component.html',
-  styles: ``
+  templateUrl: './anhadir.component.html'
 })
 export class PaginaAnhadirComponent {
+
+  public formularioHeroe = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>('')
+  });
 
   public publicadores = [
     { id: 'DC Comics', desc: 'DC - Comics'},
