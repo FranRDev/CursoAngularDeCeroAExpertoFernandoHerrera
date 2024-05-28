2e889e3495eb00c2ebe4cb195deda42a5f34aa5e
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-28 12:53:41 +0200

    Clase 215. Establecer el valor del formulario

--- 06-heroes-app/src/app/heroes/pages/anhadir/anhadir.component.ts
+++ 06-heroes-app/src/app/heroes/pages/anhadir/anhadir.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Heroe, Publisher } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'pagina-anhadir',
   templateUrl: './anhadir.component.html'
 })
-export class PaginaAnhadirComponent {
+export class PaginaAnhadirComponent implements OnInit {
 
   public formularioHeroe = new FormGroup({
     id: new FormControl<string>(''),
@@ -20,11 +22,27 @@ export class PaginaAnhadirComponent {
   });
 
   public publicadores = [
-    { id: 'DC Comics', desc: 'DC - Comics'},
-    { id: 'Marvel Comics', desc: 'Marvel - Comics'}
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ];
 
-  constructor(private servicioHeroes: HeroesService) { }
+  constructor(
+    private servicioHeroes: HeroesService,
+    private rutaActiva: ActivatedRoute,
+    private enrutador: Router
+  ) { }
+
+  ngOnInit(): void {
+    if (!this.enrutador.url.includes('editar')) { return; }
+
+    this.rutaActiva.params
+      .pipe(switchMap(({ id }) => this.servicioHeroes.obtenerHeroePorId(id)))
+      .subscribe(heroe => {
+        if (!heroe) { return this.enrutador.navigateByUrl('/'); }
+        this.formularioHeroe.reset(heroe);
+        return;
+      });
+  }
 
   get heroeActual(): Heroe {
     const heroe = this.formularioHeroe.value as Heroe;
@@ -35,10 +53,10 @@ export class PaginaAnhadirComponent {
     if (this.formularioHeroe.invalid) { return; }
 
     if (this.heroeActual.id) {
-      this.servicioHeroes.actualizarHeroe(this.heroeActual).subscribe(heroe => {}); // TODO: Mostrar snakbar
+      this.servicioHeroes.actualizarHeroe(this.heroeActual).subscribe(heroe => { }); // TODO: Mostrar snakbar
 
     } else {
-      this.servicioHeroes.anhadirHeroe(this.heroeActual).subscribe(heroe => {}); // TODO: ...
+      this.servicioHeroes.anhadirHeroe(this.heroeActual).subscribe(heroe => { }); // TODO: ...
     }
   }
 
