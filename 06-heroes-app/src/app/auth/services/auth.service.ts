bf59bb6e1d64ff30beb9e482161d53406bbb8f19
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-28 13:55:09 +0200

    Clase 223. AuthService - Servicio de Autenticación

---
+++ 06-heroes-app/src/app/auth/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+import { environments } from '../../../environments/environments';
+import { User } from '../interfaces/usuario.interface';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  private urlBase = environments.urlBase;
+  private usuario?: User;
+
+  constructor(private clienteHttp: HttpClient) { }
+
+  get usuarioActual(): User | undefined {
+    if (!this.usuario) { return undefined; }
+    return structuredClone(this.usuario);
+  }
+
+  inicioSesion(correo: string, clave: string): Observable<User> {
+    return this.clienteHttp.get<User>(`${this.urlBase}/users/1`)
+      .pipe(
+        tap(usuario => this.usuario = usuario),
+        tap(usuario => localStorage.setItem('token', usuario.id.toString()))
+      );
+  }
+
+}
