586fd7d508a2eb3c0c78d9b16206eca8ebd24425
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-29 08:05:13 +0200

    Clase 234. Estructura HTML de las páginas

---
+++ 07-formularios-app/src/app/reactivos/pages/basicos/basicos.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({ templateUrl: './basicos.component.html' })
+export class PaginaBasicaComponent {
+
+}
--- 07-formularios-app/src/app/reactivos/reactivos.module.ts
+++ 07-formularios-app/src/app/reactivos/reactivos.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactivosRoutingModule } from './reactivos-routing.module';
+import { PaginaBasicaComponent } from './pages/basicos/basicos.component';
+import { DinamicaComponent } from './pages/dinamicos/dinamicos.component';
+import { InterruptoresComponent } from './pages/interruptores/interruptores.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaginaBasicaComponent,
+    DinamicaComponent,
+    InterruptoresComponent
+  ],
   imports: [
     CommonModule,
     ReactivosRoutingModule
