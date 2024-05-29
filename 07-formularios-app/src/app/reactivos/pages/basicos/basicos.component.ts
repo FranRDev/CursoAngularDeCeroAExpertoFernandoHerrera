c0d8fc4c448c12714bc19c0a2675ff2e22b78f69
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-29 08:40:30 +0200

    Clase 237. Primeros pasos en formularios reactivos

--- 07-formularios-app/src/app/reactivos/pages/basicos/basicos.component.ts
+++ 07-formularios-app/src/app/reactivos/pages/basicos/basicos.component.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgModel } from '@angular/forms';
 
 @Component({ templateUrl: './basicos.component.html' })
 export class PaginaBasicosComponent {
+
+  // public formulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl(''),
+  //   precio: new FormControl(0),
+  //   existencias: new FormControl(0)
+  // });
+
+  public formulario: FormGroup = this.fb.group({
+    nombre: [''],
+    precio: [0],
+    existencias: [0]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  guardar(): void {
+    console.log(this.formulario.value);
+  }
 
 }
--- 07-formularios-app/src/app/reactivos/reactivos.module.ts
+++ 07-formularios-app/src/app/reactivos/reactivos.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { ReactivosRoutingModule } from './reactivos-routing.module';
 import { PaginaBasicosComponent } from './pages/basicos/basicos.component';
 import { PaginaDinamicosComponent } from './pages/dinamicos/dinamicos.component';
 import { PaginaInterruptoresComponent } from './pages/interruptores/interruptores.component';
+import { ReactivosRoutingModule } from './reactivos-routing.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { PaginaInterruptoresComponent } from './pages/interruptores/interruptore
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactivosRoutingModule
   ]
 })
