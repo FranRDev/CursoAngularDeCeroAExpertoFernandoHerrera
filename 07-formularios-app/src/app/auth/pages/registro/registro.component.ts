110bbbad4b2bbeb3a6fd56392afb6c9fc92410f7
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-29 11:38:57 +0200

    Clase 252. Formulario de Registro

--- 07-formularios-app/src/app/auth/pages/registro/registro.component.ts
+++ 07-formularios-app/src/app/auth/pages/registro/registro.component.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({ templateUrl: './registro.component.html' })
 export class PaginaRegistroComponent {
+
+  public formulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required]],
+    correo: ['', [Validators.required]],
+    usuario: ['', [Validators.required]],
+    clave: ['', [Validators.required]],
+    claveRepetida: ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  esCampoValido(campo: string) {
+    // TODO: Obtener de servicio
+  }
+
+  guardar(): void {
+    this.formulario.markAllAsTouched();
+  }
 
 }
