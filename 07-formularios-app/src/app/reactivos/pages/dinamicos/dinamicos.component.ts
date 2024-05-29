f342ac800548484d3a0cb1adf5bc6d87e9e845a2
FranRDev <franciscorodriguezgar@gmail.com>
2024-05-29 09:25:36 +0200

    Clase 242. Formularios dinámicos

--- 07-formularios-app/src/app/reactivos/pages/dinamicos/dinamicos.component.ts
+++ 07-formularios-app/src/app/reactivos/pages/dinamicos/dinamicos.component.ts
@@ -1,6 +1,31 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({ templateUrl: './dinamicos.component.html' })
 export class PaginaDinamicosComponent {
+
+  public formulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    juegosFavoritos: this.fb.array([
+      ['Pang', Validators.required],
+      ['Pacman', Validators.required]
+    ])
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  get juegosFavoritos() {
+    return this.formulario.get('juegosFavoritos') as FormArray;
+  }
+
+  guardar(): void {
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.formulario.value);
+    this.formulario.reset();
+  }
 
 }
