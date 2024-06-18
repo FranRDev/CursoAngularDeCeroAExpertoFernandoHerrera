6e2c2b6fbefc018eee7729a14dafe6aae634352e
FranRDev <franciscorodriguezgar@gmail.com>
2024-06-18 14:31:52 +0200

    Clase 288. Menú de la aplicación

--- 10-mapas-app/src/app/mapas/components/menu-lateral/menu-lateral.component.ts
+++ 10-mapas-app/src/app/mapas/components/menu-lateral/menu-lateral.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface ElementoMenu {
+  nombre: string;
+  ruta: string;
+}
+
 @Component({
-  selector: 'app-menu-lateral',
+  selector: 'menu-lateral',
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.css'
 })
 export class MenuLateralComponent {
+
+  public elementosMenu: ElementoMenu[] = [
+    { ruta: '/mapas/pantalla-completa', nombre: 'Pantalla completa' },
+    { ruta: '/mapas/rango-zoom', nombre: 'Rango - Zoom' },
+    { ruta: '/mapas/marcadores', nombre: 'Marcadores' },
+    { ruta: '/mapas/propiedades', nombre: 'Casas' },
+  ];
 
 }
