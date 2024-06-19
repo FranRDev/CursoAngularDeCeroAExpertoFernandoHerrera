9e5c4b927201e4165dfcb49abcc06041e82f755b
FranRDev <franciscorodriguezgar@gmail.com>
2024-06-19 13:40:51 +0200

    Clase 294. Controlar el nivel del Zoom

--- 10-mapas-app/src/app/mapas/pages/rango-zoom/rango-zoom.component.ts
+++ 10-mapas-app/src/app/mapas/pages/rango-zoom/rango-zoom.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 import { Map } from 'mapbox-gl';
+import { map } from 'rxjs';
 
 @Component({
   templateUrl: './rango-zoom.component.html',
@@ -10,15 +11,45 @@ export class PaginaRangoZoomComponent implements AfterViewInit {
 
   @ViewChild('mapa') divMapa?: ElementRef;
 
+  public mapa?: Map;
+  public zoom: number = 10;
+
   ngAfterViewInit(): void {
     if (!this.divMapa) { throw 'Elemento HTML no encontrado'; }
 
-    const mapa = new Map({
+    this.mapa = new Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40], // [lng, lat]
-      zoom: 9,
+      zoom: this.zoom,
     });
+
+    this.escuchadoresMapa();
+  }
+
+  escuchadoresMapa(): void {
+    if (!this.mapa) { throw 'Mapa no inicializado'; }
+
+    this.mapa.on('zoom', () => {
+      this.zoom = this.mapa!.getZoom();
+    });
+
+    this.mapa.on('zoomend', () => {
+      if (this.mapa!.getZoom() > 18) { this.mapa!.zoomTo(18); }
+    });
+  }
+
+  ampliarZoom(): void {
+    this.mapa?.zoomIn();
+  }
+
+  reducirZoom(): void {
+    this.mapa?.zoomOut();
+  }
+
+  zoomCambiado(valor: string): void {
+    this.zoom = Number(valor);
+    this.mapa!.zoomTo(this.zoom);
   }
 
 }
