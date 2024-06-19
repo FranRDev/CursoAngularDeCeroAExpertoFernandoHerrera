a3eca58ef41131c333b4e7de634e7704c09fc51b
FranRDev <franciscorodriguezgar@gmail.com>
2024-06-19 13:05:32 +0200

    Clase 291. Mostrar un mapa en pantalla completa

--- 10-mapas-app/src/app/mapas/pages/pantalla-completa/pantalla-completa.component.ts
+++ 10-mapas-app/src/app/mapas/pages/pantalla-completa/pantalla-completa.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = '';
 
 @Component({
   templateUrl: './pantalla-completa.component.html',
   styleUrl: './pantalla-completa.component.css'
 })
-export class PaginaPantallaCompletaComponent {
+export class PaginaPantallaCompletaComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: 'mapa', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+  }
 
 }
