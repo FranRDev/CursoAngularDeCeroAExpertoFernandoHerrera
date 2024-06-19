7a593674adc5ae9d81b761a4be98fd820b270315
FranRDev <franciscorodriguezgar@gmail.com>
2024-06-19 13:56:58 +0200

    Clase 295. Obtener las coordenadas centrales del mapa

--- 10-mapas-app/src/app/mapas/pages/rango-zoom/rango-zoom.component.ts
+++ 10-mapas-app/src/app/mapas/pages/rango-zoom/rango-zoom.component.ts
@@ -1,18 +1,19 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
-import { Map } from 'mapbox-gl';
+import { LngLat, Map } from 'mapbox-gl';
 import { map } from 'rxjs';
 
 @Component({
   templateUrl: './rango-zoom.component.html',
   styleUrl: './rango-zoom.component.css'
 })
-export class PaginaRangoZoomComponent implements AfterViewInit {
+export class PaginaRangoZoomComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa?: ElementRef;
 
   public mapa?: Map;
   public zoom: number = 10;
+  public lngLat: LngLat = new LngLat(-112.04, 46.62);
 
   ngAfterViewInit(): void {
     if (!this.divMapa) { throw 'Elemento HTML no encontrado'; }
@@ -20,11 +21,15 @@ export class PaginaRangoZoomComponent implements AfterViewInit {
     this.mapa = new Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-74.5, 40], // [lng, lat]
+      center: this.lngLat, // [lng, lat]
       zoom: this.zoom,
     });
 
     this.escuchadoresMapa();
+  }
+
+  ngOnDestroy(): void {
+    this.mapa?.remove();
   }
 
   escuchadoresMapa(): void {
@@ -37,6 +42,10 @@ export class PaginaRangoZoomComponent implements AfterViewInit {
     this.mapa.on('zoomend', () => {
       if (this.mapa!.getZoom() > 18) { this.mapa!.zoomTo(18); }
     });
+
+    this.mapa.on('move', () => {
+      this.lngLat = this.mapa!.getCenter();
+    })
   }
 
   ampliarZoom(): void {
