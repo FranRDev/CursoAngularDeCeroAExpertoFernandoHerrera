1c9e4798b3f25a768ed669dcc473fdd924cf0dd3
FranRDev <franciscorodriguezgar@gmail.com>
2024-06-20 13:43:02 +0200

    Clase 308. Standalone - Como Página

--- 10-mapas-app/src/app/app-routing.module.ts
+++ 10-mapas-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'mapas', loadChildren: () => import('./mapas/mapas.module').then(m => m.MapasModule) },
+  { path: 'solo', loadComponent: () => import('./solo/pages/solo/solo.component').then(c => c.PaginaSoloComponent) },
   { path: '**', redirectTo: 'mapas' }
 ];
 
