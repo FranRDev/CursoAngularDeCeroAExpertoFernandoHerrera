79d15ab3ca5f38bfe3dda5b17719af8ca70403a2
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-08 10:29:17 +0200

    Clase 321. Cambiar el mensaje de la etiqueta

--- 11-directivas-senhales/src/app/shared/directivas/etiqueta-personalizada.directive.ts
+++ 11-directivas-senhales/src/app/shared/directivas/etiqueta-personalizada.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[etiquetaPersonalizada]'
@@ -10,8 +11,15 @@ export class EtiquetaPersonalizadaDirective implements OnInit {
     this.establecerEstilo();
   }
 
+  @Input() set errores(valor: ValidationErrors | null | undefined) {
+    this._errores = valor;
+    console.log(this._errores);
+    this.establecerMensajeError();
+  }
+
   private elementoHtml?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errores?: ValidationErrors | null | undefined;
 
   constructor(private elemento: ElementRef<HTMLElement>) {
     // console.log('Directiva constructor');
@@ -27,6 +35,32 @@ export class EtiquetaPersonalizadaDirective implements OnInit {
   establecerEstilo(): void {
     if (!this.elementoHtml) { return; }
     this.elementoHtml!.nativeElement.style.color = this._color;
+  }
+
+  establecerMensajeError(): void {
+    if (!this.elementoHtml) { return; }
+
+    if (!this._errores) {
+      this.elementoHtml.nativeElement.innerText = 'No hay errores';
+      return;
+    }
+
+    const errores = Object.keys(this._errores);
+
+    if (errores.includes('required')) {
+      this.elementoHtml.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+
+    if (errores.includes('minlength')) {
+      this.elementoHtml.nativeElement.innerText = 'Debe tener más de 6 caracteres';
+      return;
+    }
+
+    if (errores.includes('email')) {
+      this.elementoHtml.nativeElement.innerText = 'El formato de correo no es válido';
+      return;
+    }
   }
 
 }
