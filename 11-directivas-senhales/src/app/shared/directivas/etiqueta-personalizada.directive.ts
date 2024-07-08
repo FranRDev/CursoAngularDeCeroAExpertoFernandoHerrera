dbeae6869adb4b66e68032ae77a90185d6eff36f
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-08 09:32:50 +0200

    Clase 320. Directive Input - Cambiar el color del host

--- 11-directivas-senhales/src/app/shared/directivas/etiqueta-personalizada.directive.ts
+++ 11-directivas-senhales/src/app/shared/directivas/etiqueta-personalizada.directive.ts
@@ -1,19 +1,32 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[etiquetaPersonalizada]'
 })
 export class EtiquetaPersonalizadaDirective implements OnInit {
 
+  @Input() set color(valor: string) {
+    this._color = valor;
+    this.establecerEstilo();
+  }
+
   private elementoHtml?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
 
   constructor(private elemento: ElementRef<HTMLElement>) {
     // console.log('Directiva constructor');
+    // console.log(elemento);
     this.elementoHtml = elemento;
   }
 
   ngOnInit(): void {
     console.log('Directiva OnInit');
+    this.establecerEstilo();
+  }
+
+  establecerEstilo(): void {
+    if (!this.elementoHtml) { return; }
+    this.elementoHtml!.nativeElement.style.color = this._color;
   }
 
 }
