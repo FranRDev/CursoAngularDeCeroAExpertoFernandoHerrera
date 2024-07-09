7622d8016390f3cb90ed5ca632cf730dba2bfbd3
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-09 14:31:12 +0200

    Clase 333. Estructura de pantalla

--- 11-directivas-senhales/src/app/senhales/pages/propiedades/propiedades.component.ts
+++ 11-directivas-senhales/src/app/senhales/pages/propiedades/propiedades.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
 })
 export class PaginaPropiedadesComponent {
 
+  campoActualizado(campo: string, valor: string) {
+    console.log({ campo, valor });
+  }
+
 }
