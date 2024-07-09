2a3cfff944fc49a1592ae5995cbbd0f560dbf7b7
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-09 13:35:18 +0200

    Clase 329. Counter Signal

--- 11-directivas-senhales/src/app/senhales/pages/contador/contador.component.ts
+++ 11-directivas-senhales/src/app/senhales/pages/contador/contador.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './contador.component.html',
   styleUrl: './contador.component.css'
 })
 export class PaginaContadorComponent {
+
+  public contador = signal(10);
+  public contadorAlCuadrado = computed(() => this.contador() * this.contador());
+
+  incrementar(valor: number): void {
+    this.contador.update(c => c + valor);
+  }
 
 }
