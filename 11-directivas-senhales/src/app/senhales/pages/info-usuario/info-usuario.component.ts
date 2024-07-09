62eeedc86414c76784340e6a59f76b24c64d6ca6
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-09 14:25:31 +0200

    Clase 332. Manejo de errores y propiedades computadas

--- 11-directivas-senhales/src/app/senhales/pages/info-usuario/info-usuario.component.ts
+++ 11-directivas-senhales/src/app/senhales/pages/info-usuario/info-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ServicioUsuariosService as UsuariosService } from '../../services/usuarios.service';
 import { Usuario } from '../../interfaces/usuario-respuesta.interface';
 
@@ -12,6 +12,10 @@ export class PaginaInfoUsuarioComponent implements OnInit {
   public idUsuario = signal(1);
   public usuarioActual = signal<Usuario | undefined>(undefined);
   public usuarioEncontrado = signal(true);
+  public nombreCompleto = computed<string>(() => {
+    if (!this.usuarioActual()) { return 'Usuario no encontrado'; }
+    return `${this.usuarioActual()!.first_name} ${this.usuarioActual()!.last_name}`;
+  });
 
   ngOnInit(): void {
     this.cargarUsuario(this.idUsuario());
@@ -21,7 +25,16 @@ export class PaginaInfoUsuarioComponent implements OnInit {
     if (id < 1) { return; }
     this.usuarioActual.set(undefined);
     this.idUsuario.set(id);
-    this.servicioUsuarios.obtenerUsuarioPorId(id).subscribe(u => this.usuarioActual.set(u));
+    this.servicioUsuarios.obtenerUsuarioPorId(id).subscribe({
+      next: (u) => {
+        this.usuarioActual.set(u);
+        this.usuarioEncontrado.set(true);
+      },
+      error: () => {
+        this.usuarioEncontrado.set(false);
+        this.usuarioActual.set(undefined);
+      }
+    });
   }
 
 }
