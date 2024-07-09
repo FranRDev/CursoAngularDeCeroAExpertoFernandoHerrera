4d2b06adface7f2f2e15a1cc25f9e98db84ca5b9
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-09 14:43:37 +0200

    Clase 334. Mutaciones - Actualizar parte del estado

--- 11-directivas-senhales/src/app/senhales/pages/propiedades/propiedades.component.ts
+++ 11-directivas-senhales/src/app/senhales/pages/propiedades/propiedades.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { Usuario } from '../../interfaces/usuario-respuesta.interface';
 
 @Component({
   templateUrl: './propiedades.component.html',
@@ -6,8 +7,43 @@ import { Component } from '@angular/core';
 })
 export class PaginaPropiedadesComponent {
 
-  campoActualizado(campo: string, valor: string) {
-    console.log({ campo, valor });
+  public usuario = signal<Usuario>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  public nombreCompleto = computed<string>(() => `${this.usuario()!.first_name} ${this.usuario()!.last_name}`);
+
+  campoActualizado(campo: keyof Usuario, valor: string) {
+    this.usuario.update(u => {
+      switch (campo) {
+        case 'avatar':
+          u.avatar = valor;
+          break;
+
+        case 'email':
+          u.email = valor;
+          break;
+
+        case 'first_name':
+          u.first_name = valor;
+          break;
+
+        case 'id':
+          u.id = Number(valor);
+          break;
+
+        case 'last_name':
+          u.last_name = valor;
+          break;
+      }
+
+      return u;
+    })
+
   }
 
 }
