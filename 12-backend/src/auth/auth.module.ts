328e57a54d5efd5f52c04081fc760e7a4e7295c4
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-10 13:44:19 +0200

    Clase 344. Crear modelo y esquema

--- 12-backend/src/auth/auth.module.ts
+++ 12-backend/src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { EsquemaUsuario, Usuario } from './entities/usuario.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Usuario.name, schema: EsquemaUsuario}])],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ 12-backend/src/auth/entities/usuario.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Usuario {
+    // _id: string;
+    @Prop({ required: true })
+    nombre: string;
+    @Prop({ unique: true, required: true })
+    correo: string;
+    @Prop({ minlength: 6, required: true })
+    clave: string;
+    @Prop({ type: [String], default: ['usuario'] })
+    roles: string[];
+    @Prop({ default: true })
+    activo: boolean;
+}
+
+export const EsquemaUsuario = SchemaFactory.createForClass(Usuario);
