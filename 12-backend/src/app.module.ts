cc164ec4e743cfaac5fc7a7b0a865c653f7cd479
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-10 13:02:59 +0200

    Clase 342. Conectar Nest con MongoDB

--- 12-backend/src/app.module.ts
+++ 12-backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017')
+  ],
   controllers: [],
   providers: [],
 })
