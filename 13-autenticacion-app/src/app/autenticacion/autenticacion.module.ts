045b45dac3205340ad289bdfc27fbff8257b7083
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-11 10:58:41 +0200

    Clase 362. Formulario de inicio de sesión

--- 13-autenticacion-app/src/app/autenticacion/autenticacion.module.ts
+++ 13-autenticacion-app/src/app/autenticacion/autenticacion.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AutenticacionLayoutComponent } from './layouts/autenticacion-layout/autenticacion-layout.component';
 import { AutenticacionRoutingModule } from './autenticacion-routing.module';
 import { PaginaInicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
 import { PaginaRegistroComponent } from './pages/registro/registro.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { PaginaRegistroComponent } from './pages/registro/registro.component';
   ],
   imports: [
     AutenticacionRoutingModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class AutenticacionModule { }
--- 13-autenticacion-app/src/app/autenticacion/pages/inicio-sesion/inicio-sesion.component.ts
+++ 13-autenticacion-app/src/app/autenticacion/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
 export class PaginaInicioSesionComponent {
+
+  private fb = inject(FormBuilder);
+
+  public formulario: FormGroup = this.fb.group({
+    correo: ['', [Validators.required, Validators.email]],
+    clave: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  inicioSesion() {
+    console.log(this.formulario.value)
+  }
 
 }
