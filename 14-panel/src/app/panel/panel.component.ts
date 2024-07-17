d3120caa82f34a3d528c07dbd8b80bb35d528ef2
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-17 12:43:42 +0200

    Clase 394. Configuración de dashboard - Tailwind

--- 14-panel/src/app/panel/panel.component.ts
+++ 14-panel/src/app/panel/panel.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MenuLateralComponent } from '../shared/menu-lateral/menu-lateral.component';
+
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MenuLateralComponent],
   templateUrl: './panel.component.html',
   styles: ``
 })
--- 14-panel/src/app/shared/menu-lateral/menu-lateral.component.ts
+++ 14-panel/src/app/shared/menu-lateral/menu-lateral.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 
+import { routes } from '../../app.routes';
+
 @Component({
-  selector: 'app-menu-lateral',
+  selector: 'menu-lateral',
   standalone: true,
   imports: [],
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.css'
 })
 export class MenuLateralComponent {
+
+  public elementosMenu = routes
+    .map(ruta => ruta.children ?? [])
+    .flat()
+    .filter(ruta => ruta && ruta.path)
+    .filter(ruta => !ruta.path?.includes(':'));
+
+  // constructor() {
+  //   const rutas = routes
+  //     .map(ruta => ruta.children ?? [])
+  //     .flat()
+  //     .filter(ruta => ruta && ruta.path)
+  //     .filter(ruta => !ruta.path?.includes(':'));
+  // }
 
 }
