64efeb9b90f39441836314162df669e0c5152cd2
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-17 13:53:22 +0200

    Clase 401. ChangeDetection - OnPush

--- 14-panel/src/app/panel/pages/deteccion-cambios/deteccion-cambios.component.ts
+++ 14-panel/src/app/panel/pages/deteccion-cambios/deteccion-cambios.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+import { TituloComponent } from '@/shared/titulo/titulo.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './deteccion-cambios.component.html',
+  imports: [CommonModule, TituloComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <titulo [titulo]="frameworkActual()" />
+    <pre>{{ frameworkSenhal() | json }}</pre>
+    <pre>{{ frameworkPropiedad | json }}</pre>
+  `,
   styles: ``
 })
 export default class DeteccionCambiosComponent {
+
+  frameworkActual = computed(() => `Detección de cambio - ${this.frameworkSenhal().nombre}`);
+  frameworkSenhal = signal({ nombre: 'Angular', lanzamiento: 2016 });
+  frameworkPropiedad = { nombre: 'Angular', lanzamiento: 2016 };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkPropiedad.nombre = 'React';
+      this.frameworkSenhal.update(valor => ({ ...valor, nombre: 'React' }));
+      console.log('Hecho');
+    }, 3000);
+  }
 
 }
