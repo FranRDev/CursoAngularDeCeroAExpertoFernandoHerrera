0f5a49f0df9f85a838a8e760c41f9a11830a6149
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-19 08:21:51 +0200

    Clase 404. Defer Triggers - Interacciones

--- 14-panel/src/app/panel/pages/opciones-diferir/opciones-diferir.component.ts
+++ 14-panel/src/app/panel/pages/opciones-diferir/opciones-diferir.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import { CargadoresPesadosRapidosComponent } from '@shared/cargadores-pesados/cargadores-pesados-rapidos.component';
 import { TituloComponent } from '@shared/titulo/titulo.component';
 
 @Component({
   standalone: true,
-  imports: [TituloComponent],
+  imports: [TituloComponent, CargadoresPesadosRapidosComponent, TituloComponent],
   templateUrl: './opciones-diferir.component.html',
   styles: ``
 })
--- 14-panel/src/app/shared/cargadores-pesados/cargadores-pesados-rapidos.component.ts
+++ 14-panel/src/app/shared/cargadores-pesados/cargadores-pesados-rapidos.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cargadores-pesados-rapidos',
   standalone: true,
-  imports: [],
-  template: `<h1>Hola mundo</h1>`
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full h-[600px]', claseCss]">
+      <ng-content />
+    </section>
+  `
 })
 export class CargadoresPesadosRapidosComponent {
+
+  @Input({ required: true }) claseCss!: string;
+
+  constructor() {
+    console.log('CargadoresPesadosRapidosComponent creado');
+  }
 
 }
