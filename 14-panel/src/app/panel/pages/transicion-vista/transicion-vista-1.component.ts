ee641a3e81860b2836f3754bae0f83bd42d04dc5
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-19 08:45:33 +0200

    Clase 406. ViewTransitions API

---
+++ 14-panel/src/app/panel/pages/transicion-vista/transicion-vista-1.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { TituloComponent } from '@shared/titulo/titulo.component';
+
+@Component({
+  standalone: true,
+  imports: [TituloComponent],
+  template: `
+    <titulo titulo="Transición de vista 1" />
+
+    <section class="flex justify-start">
+      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300" />
+      <div class="bg-blue-500 w-56 h-56"></div>
+    </section>
+  `
+})
+export default class TransicionVistaComponent {
+
+}
