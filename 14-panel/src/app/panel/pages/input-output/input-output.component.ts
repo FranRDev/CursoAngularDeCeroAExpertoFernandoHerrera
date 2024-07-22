a1a743ccb494802dd8d7b14ef5137eac485c554c
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-22 08:12:17 +0200

    Clase 417. Añadir productos dinámicamente

--- 14-panel/src/app/panel/pages/input-output/input-output.component.ts
+++ 14-panel/src/app/panel/pages/input-output/input-output.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TarjetaProductoComponent } from './ui/tarjeta-producto/tarjeta-producto.component';
+import { Producto } from '@interfaces/producto.interface';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
   selector: 'input-output',
@@ -10,6 +12,26 @@ import { TarjetaProductoComponent } from './ui/tarjeta-producto/tarjeta-producto
   templateUrl: './input-output.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class InputOutputComponent {
+export default class InputOutputComponent implements OnDestroy {
+  productos = signal<Producto[]>([
+    { id: 1, nombre: `Producto 1`, cantidad: 0 },
+    { id: 2, nombre: `Producto 2`, cantidad: 0 }
+  ]);
+
+  private intervaloSuscripcion = interval(1000)
+    .pipe(
+      tap(() => {
+        this.productos.update((productos) => [
+          ...productos,
+          { id: productos.length + 1, nombre: `Producto ${productos.length + 1}`, cantidad: 0 }
+        ])
+      }),
+      take(7)
+    )
+    .subscribe();
+
+    ngOnDestroy(): void {
+      this.intervaloSuscripcion.unsubscribe();
+    }
 
 }
