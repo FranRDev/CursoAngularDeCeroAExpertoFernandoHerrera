e1f396dcee52e67f32b3300118b9209e8f0623be
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-22 08:51:48 +0200

    Clase 520. Usar componentes de Angular Material

--- 14-panel/src/app/panel/pages/material/material.component.ts
+++ 14-panel/src/app/panel/pages/material/material.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'material',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
