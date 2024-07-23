44ce9c99aa48cdfb402a0c2491a1eac8c5a4e90c
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-23 12:33:05 +0200

    Clase 446. Obtener la ruta entre dos puntos

---
+++ 15-bonus/src/app/mapas/api/direcciones.api-client.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHandler } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+import { environment } from "../../../environments/environment";
+
+@Injectable({ providedIn: 'root' })
+export class ClienteApiDirecciones extends HttpClient {
+
+  urlBase: string = 'https://api.mapbox.com/directions/v5/mapbox/driving';
+
+  constructor(handler: HttpHandler) {
+    super(handler);
+  }
+
+  public override get<T>(url: string) {
+    url = this.urlBase + url;
+
+    return super.get<T>(url, {
+      params: {
+        alternatives: false,
+        geometries: 'geojson',
+        language: 'es',
+        overview: 'simplified',
+        steps: false,
+        access_token: environment.mapboxToken
+      }
+    });
+  }
+
+}
