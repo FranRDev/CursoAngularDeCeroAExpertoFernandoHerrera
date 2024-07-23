edb0cfe1e06e824f5295020bdb5150a6d1f58a84
FranRDev <franciscorodriguezgar@gmail.com>
2024-07-23 09:16:05 +0200

    Clase 439. Diseño de componentes de búsqueda y detalles

--- 15-bonus/src/app/mapas/mapas.module.ts
+++ 15-bonus/src/app/mapas/mapas.module.ts
@@ -6,9 +6,11 @@ import { VistaMapaComponent } from './components/vista-mapa/vista-mapa.component
 import { CargaComponent } from './components/carga/carga.component';
 import { BotonMiLocalizacionComponent } from './components/boton-mi-localizacion/boton-mi-localizacion.component';
 import { LogoAngularComponent } from './components/logo-angular/logo-angular.component';
+import { BarraBusquedaComponent } from './components/barra-busqueda/barra-busqueda.component';
+import { ResultadosBusquedaComponent } from './components/resultados-busqueda/resultados-busqueda.component';
 
 @NgModule({
-  declarations: [PaginaMapaComponent, VistaMapaComponent, CargaComponent, BotonMiLocalizacionComponent, LogoAngularComponent],
+  declarations: [PaginaMapaComponent, VistaMapaComponent, CargaComponent, BotonMiLocalizacionComponent, LogoAngularComponent, BarraBusquedaComponent, ResultadosBusquedaComponent],
   imports: [CommonModule],
   exports: [PaginaMapaComponent]
 })
